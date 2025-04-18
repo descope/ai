9a95c3277fe26d349feaa7374885696a52438c03
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2025-04-17 20:44:05 -0700

    Clean up Express sample

--- examples/mcp-server-express-vercel/src/index.ts
+++ examples/mcp-server-express-vercel/src/index.ts
@@ -8,16 +8,13 @@ import { descopeMcpAuthRouter, descopeMcpBearerAuth, DescopeMcpProvider, Descope
 import cors from "cors";
 import path from 'path';
 import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
-
-// declare module "express-serve-static-core" {
-//     interface Request {
-//         /**
-//          * Information about the validated access token, if the `descopeMcpBearerAuth` middleware was used.
-//          * Contains user information and token details after successful authentication.
-//          */
-//         auth?: AuthInfo;
-//     }
-// }
+declare global {
+    namespace Express {
+        interface Request {
+            auth?: AuthInfo;
+        }
+    }
+}
 
 dotenv.config();
 
@@ -43,18 +40,14 @@ app.use(["/mcp"], descopeMcpBearerAuth());
 const { server } = createServer();
 
 app.all('/mcp', async (req, res) => {
-    console.log("mcp request", req.body);
     // Disable session tracking by setting sessionIdGenerator to undefined  
     const transport = new StreamableHTTPServerTransport({
         sessionIdGenerator: undefined
     });
-    console.log("transport", transport);
 
     // Connect to server and handle the request
     await server.connect(transport);
-    console.log("transport connected to server");
     await transport.handleRequest(req, res);
-    console.log("transport handled request");
 });
 
 const PORT = process.env.PORT || 3000;

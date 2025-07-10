2fc5b781b78575c3b070e0961ec559f12ff7c2fc
Kevin J Gao <32936811+gaokevin1@users.noreply.github.com>
2025-07-09 20:39:43 -0700

    added files

---
+++ examples/express-vercel-mcp-server/.well-known/oauth-protected-resource/route.ts
@@ -0,0 +1,32 @@
+export async function GET(req: Request) {
+  const origin = new URL(req.url).origin;
+
+  return Response.json(
+    {
+      resource: `${origin}`,
+      authorization_servers: [`${origin}`],
+      // scopes_supported: [],
+      resource_name: "Descope MCP Server",
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, mcp-protocol-version",
+      },
+    }
+  );
+}
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, mcp-protocol-version",
+      "Content-Type": "application/json",
+    },
+  });
+}
